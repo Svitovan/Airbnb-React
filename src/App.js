9cc3adad170ff02e8c1cc90b1b0531682ca29fa7
WorkPC <svitovan@outlook.com>
2022-07-12 17:03:03 +0200

    +

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
+import data from './data';
 
-
-function App() {
+export default function App() {
+  const cards = data.map(item => {
+      return (
+          <Card
+              key={item.id}
+              item={item}
+              
+            //   img={item.coverImg}
+            //   rating={item.stats.rating}
+            //   reviewCount={item.stats.reviewCount}
+            //   location={item.location}
+            //   title={item.title}
+            //   price={item.price}
+            //   openSpots={item.openSpots}
+          />
+      )
+  })        
   
-
   return (
-    <div className="App">
-      
-      <Navbar />
-      <Hero />
-      <Card 
-        img="swim.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with K."
-        price={100}
-      />
-      <Card />
-      
-    </div>
-  );
+      <div>
+          <Navbar />
+          <Hero />
+          <section className="cards-list">
+              {cards}
+          </section>
+      </div>
+  )
 }
-
-export default App;
